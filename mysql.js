07d59c27a21ce1702959a0f13023a97c50ff5f36
John Pérez <jhonfe648@gmail.com>
2020-06-09 20:49:33 -0500

    select insert

--- mysql.js
+++ mysql.js
@@ -3,3 +3,22 @@ const sequelize = new Sequelize('mysql://root@localhost:3306/songs'); // ("type 
 
 sequelize.query('SELECT * FROM songs', {type: sequelize.QueryTypes.SELECT})
     .then(res => console.log(res));
+
+// sequelize.query('SELECT * FROM songs WHERE band = :band' , {type: sequelize.QueryTypes.SELECT,
+//     replacements: {band: 1}})
+//     .then((res)=>{
+//         console.log(res);
+//     });
+
+
+// sequelize.query('INSERT INTO songs VALUES (NULL, :name, :duration, :album, :band, :publication_date )', {replacements: {
+//     name: "band one",
+//     duration: 3,
+//     album: "fist almum",
+//     band: 1,
+//     publication_date: "22/02/89"
+// }}).then((res)=>{
+//     console.log(res);
+// })
+
+
